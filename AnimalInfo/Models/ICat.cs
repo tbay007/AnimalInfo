57f5e0de33f7c28c392db3fa91dfcc79bc5023b6
tbay007 <tb007magetech@gmail.com>
2024-08-16 07:26:00 -0400

    Checking in comments for the ICat interface.

--- AnimalInfo/Models/ICat.cs
+++ AnimalInfo/Models/ICat.cs
@@ -8,8 +8,17 @@ namespace AnimalInfo.Models
 {
     public interface ICat
     {
+        /// <summary>
+        /// ID for the cat
+        /// </summary>
         int Id { get; set; }
+        /// <summary>
+        /// Image location if there is one
+        /// </summary>
         string? ImageUrl { get; set; }
+        /// <summary>
+        /// User identification number, in case the IDs don't go chronologically
+        /// </summary>
         string? UID { get; set; }
     }
 }
