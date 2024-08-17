bb1a7afcc63fa557f045e5e43e0621735ea3b958
tbay007 <tb007magetech@gmail.com>
2024-08-17 09:10:02 -0400

    Added more comments.

--- AnimalInfo/Models/Cat.cs
+++ AnimalInfo/Models/Cat.cs
@@ -4,9 +4,17 @@ namespace AnimalInfo.Models
 {
     public class Cat : Animal, ICat
     {
-        
+            /// <summary>
+            /// ID of the cat
+            /// </summary>
             public int Id { get; set; }
+            /// <summary>
+            /// Image url of the cat, if there is one
+            /// </summary>
             public string? ImageUrl { get; set; }
+            /// <summary>
+            /// UID in case of ID numbers getting messed up
+            /// </summary>
             public string? UID { get; set; }
         
     }
